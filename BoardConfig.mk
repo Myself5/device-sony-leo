5b2b9624b585d56a77946a87cee3330295948ae9
Christian Oder <myself5@carbonrom.org>
2015-07-12 21:38:32 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,5 +26,6 @@ BUILD_KERNEL := true
 MR_DPI := xhdpi
 MR_DPI_FONT := 340
 MR_KEXEC_MEM_MIN := 0x0ff00000
+MR_DEVICE_VARIANTS := z3
 
 PRODUCT_VENDOR_KERNEL_HEADERS += device/sony/leo/kernel-headers
